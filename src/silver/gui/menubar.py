12c551760f6028cd09de8598491b0d48af5ef6d9
Petr Skovoroda <petrskovoroda@gmail.com>
2015-12-16 02:48:49 +0300

    Commented

--- src/silver/gui/menubar.py
+++ src/silver/gui/menubar.py
@@ -28,45 +28,41 @@ class Menubar(Gtk.MenuBar):
         Gtk.MenuBar.__init__(self)
         self._app = app
         self.accel_group = Gtk.AccelGroup()
-        ## Music
-        music_menu = Gtk.Menu()
-        music = Gtk.MenuItem(_("Music"))
-        music.set_submenu(music_menu)
         # Play
-        self._menubar_play = create_menuitem(_("Play"), "media-playback-start")
-        self._menubar_play.set_size_request(90, -1)
-        self._menubar_play.connect("activate", self._on_play)
+        self._play = create_menuitem(_("Play"), "media-playback-start")
+        self._play.set_size_request(90, -1)
+        self._play.connect("activate", self._on_play)
         key, mod = Gtk.accelerator_parse("F6")
-        self._menubar_play.add_accelerator("activate", self.accel_group,
+        self._play.add_accelerator("activate", self.accel_group,
                                           key, mod, Gtk.AccelFlags.VISIBLE)
         # Stop
-        self._menubar_stop = create_menuitem(_("Stop"), "media-playback-stop")
-        self._menubar_stop.set_sensitive(False)
-        self._menubar_stop.connect("activate", self._on_stop)
+        self._stop = create_menuitem(_("Stop"), "media-playback-stop")
+        self._stop.set_sensitive(False)
+        self._stop.connect("activate", self._on_stop)
         key, mod = Gtk.accelerator_parse("F7")
-        self._menubar_stop.add_accelerator("activate", self.accel_group,
+        self._stop.add_accelerator("activate", self.accel_group,
                                           key, mod, Gtk.AccelFlags.VISIBLE)
         # Record
-        self._menubar_record = create_menuitem(_("Record program"),
+        self._record = create_menuitem(_("Record program"),
                                           "media-record")
-        self._menubar_record.connect("activate", self._on_record)
+        self._record.connect("activate", self._on_record)
         key, mod = Gtk.accelerator_parse("F8")
-        self._menubar_record.add_accelerator("activate", self.accel_group,
+        self._record.add_accelerator("activate", self.accel_group,
                                           key, mod, Gtk.AccelFlags.VISIBLE)
         # Stop recording
-        self._menubar_stop_recording = create_menuitem(_("Stop recording"),
+        self._stop_recording = create_menuitem(_("Stop recording"),
                                           "media-playback-stop")
-        self._menubar_stop_recording.set_sensitive(False)
-        self._menubar_stop_recording.connect("activate", self._on_stop_record)
+        self._stop_recording.set_sensitive(False)
+        self._stop_recording.connect("activate", self._on_stop_record)
         key, mod = Gtk.accelerator_parse("F9")
-        self._menubar_stop_recording.add_accelerator("activate",
+        self._stop_recording.add_accelerator("activate",
                                           self.accel_group,
                                           key, mod, Gtk.AccelFlags.VISIBLE)
         # Mute
-        self._menubar_mute = Gtk.CheckMenuItem(_("Mute"))
-        self._menubar_mute.connect("toggled", self._on_mute)
+        self._mute = Gtk.CheckMenuItem(_("Mute"))
+        self._mute.connect("toggled", self._on_mute)
         key, mod = Gtk.accelerator_parse("<Control>M")
-        self._menubar_mute.add_accelerator("activate", self.accel_group,
+        self._mute.add_accelerator("activate", self.accel_group,
                                           key, mod, Gtk.AccelFlags.VISIBLE)
         # Refresh
         refresh = create_menuitem(_("Update schedule"), "gtk-refresh")
@@ -96,13 +92,16 @@ class Menubar(Gtk.MenuBar):
         sep = []
         for i in range(5):
             sep.append(Gtk.SeparatorMenuItem())
-        # Pack
-        for item in [ self._menubar_play,
-                      self._menubar_stop,
-                      self._menubar_record,
-                      self._menubar_stop_recording,
+        # Music Menu
+        music_menu = Gtk.Menu()
+        music = Gtk.MenuItem(_("Music"))
+        music.set_submenu(music_menu)
+        for item in [ self._play,
+                      self._stop,
+                      self._record,
+                      self._stop_recording,
                       sep[0],
-                      self._menubar_mute,
+                      self._mute,
                       sep[1],
                       refresh,
                       sep[2],
@@ -112,10 +111,6 @@ class Menubar(Gtk.MenuBar):
                       sep[4],
                       quit ]:
             music_menu.append(item)
-        ## Help
-        help_menu = Gtk.Menu()
-        help = Gtk.MenuItem(_("Help"))
-        help.set_submenu(help_menu)
         # About
         about = create_menuitem("About", "gtk-about")
         about.set_size_request(90, -1)
@@ -123,22 +118,29 @@ class Menubar(Gtk.MenuBar):
         key, mod = Gtk.accelerator_parse("F1")
         about.add_accelerator("activate", self.accel_group, key,
                               mod, Gtk.AccelFlags.VISIBLE)
+        # Help Menu
+        help_menu = Gtk.Menu()
+        help = Gtk.MenuItem(_("Help"))
+        help.set_submenu(help_menu)
         help_menu.append(about)
 
         self.append(music)
         self.append(help)
-        self.show_all()
+        self.show()
 
     def update_playback_menu(self, playing):
-        self._menubar_play.set_sensitive(not playing)
-        self._menubar_stop.set_sensitive(playing)
+        """ Update playback menu sensitivity """
+        self._play.set_sensitive(not playing)
+        self._stop.set_sensitive(playing)
 
     def update_recorder_menu(self, recording):
-        self._menubar_record.set_sensitive(not recording)
-        self._menubar_stop_recording.set_sensitive(recording)
+        """ Update recorder menu sensitivity """
+        self._record.set_sensitive(not recording)
+        self._stop_recording.set_sensitive(recording)
 
     def raise_mute(self, mute):
-        self._menubar_mute.set_active(mute)
+        """ Toggle mute checkbutton """
+        self._mute.set_active(mute)
 
     def _on_play(self, button):
         self._app.play()
